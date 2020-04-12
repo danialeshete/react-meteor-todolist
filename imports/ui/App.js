b1d4c4b70d105c81100c6739de21ecb53175db89
Danial Eshete <danial.eshete@gmail.com>
2020-04-12 18:05:52 +0200

    minimongo

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,51 +1,60 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import ReactDOM from 'react-dom';
+
+import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.js';
-import {Container} from 'react-bootstrap';
 
 // App component - represents the whole app
-export default class App extends Component {
+class App extends Component {
 
-    constructor(props) {
-        super(props);
+    handleSubmit(event) {
+        event.preventDefault();
 
-        this.getTasks = this.getTasks.bind(this);
-    }
-    getTasks() {
-        return [
-            {
-                _id: 1,
-                text: 'This is task 1'
-            }, {
-                _id: 2,
-                text: 'This is task 2'
-            }, {
-                _id: 3,
-                text: 'This is task 3'
-            }
-        ];
+        // Find the text field via the React ref
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
     renderTasks() {
-        return this
-            .getTasks()
-            .map((task) => (<Task key={task._id} task={task}/>));
+        return this.props.tasks.map((task) => (
+            <Task key={task._id} task={task} />
+            
+        ));
     }
 
     render() {
         return (
-            <Container>
-                <div className="container">
-                    <header>
-                        <h1>Todo List</h1>
-                    </header>
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
 
-                    <ul>
-                        {this.renderTasks()}
-                    </ul>
-                </div>
-            </Container>
+                <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
 
+                    <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+
+                </form>
+
+                <ul>
+                    {this.renderTasks()}
+                    {console.log(this.props.tasks)}
+                </ul>
+            </div>
         );
     }
 }
+
+export default withTracker(() => {
+    return {
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    };
+})(App);
