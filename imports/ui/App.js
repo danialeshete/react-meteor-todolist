9c0055fc36d00c8772ac3514f49e5c55bbba4ae6
Danial Eshete <danial.eshete@gmail.com>
2020-04-13 15:25:54 +0200

    its secure now but need to be changed from the client side

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,33 +1,59 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+
 
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.js';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 // App component - represents the whole app
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,               //state dass die sachen verschwunden sind
+        };
+    }
 
     handleSubmit(event) {
         event.preventDefault();
 
         // Find the text field via the React ref
-        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();            //finde textInput und nimm Value
 
-        Tasks.insert({
+        Tasks.insert({                      //insert text und date in mongodb
             text,
             createdAt: new Date(), // current time
+            owner: Meteor.userId(),           // _id of logged in user
+            username: Meteor.user().username,  // username of logged in user
         });
 
         // Clear form
-        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';       //lösche textinput value
+    }
+
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,               //änder hideCompleted state
+        });
     }
 
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;               //filtertedTask sind alle Tasks in api/tasks
+
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);       //wenn hideCompleted == true dann filtern takss nach nicht gecheckt
+        }
+
+        return filteredTasks.map((task) => (        //nimm alle gefilteret und zeig nach einander
             <Task key={task._id} task={task} />
-            
+
+
         ));
     }
 
@@ -35,14 +61,28 @@ class App extends Component {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
+
+                    <label className="hide-completed">
+                        <input
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}          //wenn gecheckt dann hideCompleted is true
+                            onClick={this.toggleHideCompleted.bind(this)}       //wenn gecklcikt dann function soll laufen 
+                        />
+                        Hide Completed Tasks
+                    </label>
+                    <AccountsUIWrapper />
+                    {this.props.currentUser ?
+                        <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                            <input
+                                type="text"
+                                ref="textInput"
+                                placeholder="Type to add new tasks"
+                            />
+                        </form> : ''
+                    }
                 </header>
-
-                <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
-
-                    <input type="text" ref="textInput" placeholder="Type to add new tasks" />
-
-                </form>
 
                 <ul>
                     {this.renderTasks()}
@@ -55,6 +95,9 @@ class App extends Component {
 
 export default withTracker(() => {
     return {
-        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(), //die neuste kommt oben
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(), //anzahl der nicht checked wird aufgezählt
+        currentUser: Meteor.user(),
+
     };
 })(App);
